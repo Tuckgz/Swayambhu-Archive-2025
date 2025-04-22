48b2aac59ace2b29425a80d6f305fb91cac7e72e
Ishmael Percy <Ishmaelpercy2004@gmail.com>
2025-04-21 22:09:06 -0400

    new mongoose schema

--- backend/nodejs-backend/models/subtitleschema.js
+++ backend/nodejs-backend/models/subtitleschema.js
@@ -3,11 +3,17 @@ import mongoose from "mongoose";
 
 // using mongoose to create a schema for the srt or vtt files in mongodb
 const subtitleFileSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  content: { type: String, required: true }, 
-  date: { type: Date, default: Date.now }
+  title: { type: String, required: true },           // filename without extension
+  filename: { type: String, required: true },        // full filename with extension
+  transcript_en: { type: String, required: true },  
+  transcript_ne: { type: String, default: "" },      
+  language: { type: String, default: "English" },    
+  url: { type: String, default: "" },              
+  speaker_name: { type: [String], default: [] },    
+  keywords: { type: [String], default: [] },         
+  date_recorded: { type: Date, default: null },      
+  date_added: { type: Date, default: Date.now }     
 });
-
 
 // allows for mongoDB indexing making our database wide word search queries faster
 subtitleFileSchema.index({ content: "text" })
