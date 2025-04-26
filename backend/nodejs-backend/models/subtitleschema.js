834f9bc836cdf682268583d22feb947ccd7556d4
Ishmael Percy <Ishmaelpercy2004@gmail.com>
2025-04-26 02:44:34 -0400

    small schema changes

--- backend/nodejs-backend/models/subtitleschema.js
+++ backend/nodejs-backend/models/subtitleschema.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const subtitleFileSchema = new mongoose.Schema({
-  title: { type: String, required: true },           // filename without extension
-  filename: { type: String, required: true },        // full filename with extension
-  transcript_en: { type: String, required: true },  
-  transcript_ne: { type: String, default: "" },      
+  title: { type: String, required: true },           
+  filename: { type: String, required: true },        
+  transcripts : {
+    en: {type: String, required: true},
+    en: { type: String, default: ""}
+  },
   language: { type: String, default: "English" },    
   url: { type: String, default: "" },              
   speaker_name: { type: [String], default: [] },    
@@ -12,13 +14,6 @@ const subtitleFileSchema = new mongoose.Schema({
   date_recorded: { type: Date, default: null },      
   date_added: { type: Date, default: Date.now }     
 });
-
-
-// transcripts: {
-//   en: { type: String, required: true },               
-//   ne: { type: String, default: "" },                 
-  
-// },
 
 subtitleFileSchema.index({ content: "text" })
 
